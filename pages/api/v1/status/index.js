250bbc6f194bd6fde483322a3ce0aca3de419b7a
thomashenrique387 <thomas.henrique@hotmail.com>
2025-11-27 08:52:03 -0300

    feat: add `database.js` and `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(req, res) {
-  res.status(200).send("Sucésso na ÃPI");
+import database from "../../../../infra/database.js";
+
+export default async function status(req, res) {
+  const result = await database.query("SELECT 1+1 AS Result");
+  console.log(result.rows);
+  res.status(200).send({ message: "status api is working!" });
 }
